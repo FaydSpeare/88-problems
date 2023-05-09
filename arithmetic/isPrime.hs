d837c0fedeffb98f96de19400afb8facf79c702d
FaydSpeare <fspea123@gmail.com>
2023-05-09 09:04:04 +0100

    arithmetic done

--- arithmetic/isPrime.hs
+++ arithmetic/isPrime.hs
@@ -20,3 +20,20 @@ primeFact n = a : primeFact (n `div` a)
 
 primeFactMult :: Int -> [(Int, Int)]
 primeFactMult n = (\x -> (head x, length x)) <$> group (primeFact n)
+
+impTotient :: Int -> Int
+impTotient n = product [ formula x | x <- primeFactMult n ]
+    where formula (y, z) = (y - 1) * (y) ^ (z - 1)
+
+primesR :: Int -> Int -> [Int]
+primesR a b = filter isPrime' [a..b]
+
+goldbach :: Int -> (Int, Int)
+goldbach n = head $ [(x, y) | x <- ps, y <- ps, x + y == n]
+    where ps = primesR 0 n
+
+goldbachList :: Int -> Int -> [(Int, Int)]
+goldbachList a b = map goldbach [x | x <- [a..b], even x]
+
+goldbachList' :: Int -> Int -> Int -> [(Int, Int)]
+goldbachList' a b c = filter (\(x, y) -> x > c && y > c) $ goldbachList a b
